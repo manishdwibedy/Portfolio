b68dc4e374a3715b7862641226255312730b5cb2
Manish Dwibedy <manish.dwibedy@gmail.com>
2026-01-14 12:50:27 +0530

    fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-  content: ["./*.html", "./*.js"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        accent: '#1e3a8a', // This fixes your contrast ratio issue (Dark Blue)
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
