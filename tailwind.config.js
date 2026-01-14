11ea09389c7b41afd98fcf1762fea384c7149f26
Manish Dwibedy <manish.dwibedy@gmail.com>
2026-01-14 11:23:48 +0530

    fixing mobile performance

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#1e3a8a', // This fixes your contrast ratio issue (Dark Blue)
+      }
+    },
+  },
+  plugins: [],
+}
