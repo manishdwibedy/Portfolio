01fe50814eb66196d373fee5152fdc4293481d94
Manish Dwibedy <manish.dwibedy@gmail.com>
2026-01-14 13:28:40 +0530

    Accessible Labels

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // If your URL is https://<user>.github.io/<repo>/, set base to '/<repo>/'
+  base: '/YOUR_REPO_NAME/', 
+})
