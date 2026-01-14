f626e5834aeed63481265b6a7792c4a5f7512c32
Manish Dwibedy <manish.dwibedy@gmail.com>
2026-01-14 13:55:09 +0530

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // If your URL is https://<user>.github.io/<repo>/, set base to '/<repo>/'
-  base: '/Portfolio/', 
+  base: '/', 
 })
